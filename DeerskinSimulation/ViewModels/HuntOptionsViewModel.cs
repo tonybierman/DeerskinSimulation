12aca3a5bc962fd5e169c19bff243b95bf48f577
Tony Bierman <tonybierman@gmail.com>
2024-07-20 07:41:54 -0500

    Hunt Options dialog

---
+++ DeerskinSimulation/ViewModels/HuntOptionsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DeerskinSimulation.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace DeerskinSimulation.ViewModels
+{
+    public class HuntOptionsViewModel
+    {
+        [Range(1, Constants.MaxPackhorses, ErrorMessage = "Packhorses must be between 1 and {1}.")]
+        public int SelectedPackhorses { get; set; } = 1;
+    }
+}

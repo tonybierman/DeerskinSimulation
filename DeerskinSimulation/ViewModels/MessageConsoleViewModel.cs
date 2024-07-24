249e1327f5128ef401688a357e24e391f80ee00b
Tony Bierman <tonybierman@gmail.com>
2024-07-24 10:07:15 -0500

    Default image

---
+++ DeerskinSimulation/ViewModels/MessageConsoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DeerskinSimulation.ViewModels
+{
+    public class MessageConsoleViewModel
+    {
+        public string ImageUrl { get; set; }
+        public string ImageCaption { get; set; }
+
+        public MessageConsoleViewModel(string imageUrl = "images/ph_256.jpg", string caption = "Deerskin Simulation") 
+        {
+            ImageUrl = imageUrl;
+            ImageCaption = caption;
+        }
+    }
+}

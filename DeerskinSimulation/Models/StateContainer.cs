3e4aa23ab63682ddb2399bcd32bef2ba02df7c54
Tony Bierman <tonybierman@gmail.com>
2024-07-24 12:51:48 -0500

    StateContainer

---
+++ DeerskinSimulation/Models/StateContainer.cs
@@ -0,0 +1,21 @@
+﻿namespace DeerskinSimulation.Models
+{
+    public class StateContainer
+    {
+        private bool? m_debug;
+
+        public bool? Debug
+        {
+            get => m_debug ?? false;
+            set
+            {
+                m_debug = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
--- DeerskinSimulation/Program.cs
+++ DeerskinSimulation/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using DeerskinSimulation;
+using DeerskinSimulation.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<StateContainer>();
 
 await builder.Build().RunAsync();
